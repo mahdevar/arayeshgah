2fc4fa15e028d83026364c0e45d2731f4e7b8a2f
Mahdevar <mahdevar@kashanu.ac.ir>
2024-10-02 09:40:04 +0330

    fix: Access port changed from 8080 to 80.

--- start.bat
+++ start.bat
@@ -3,5 +3,6 @@ SET PYTHONDONTWRITEBYTECODE=1
 set FLASK_APP=app.py
 set FLASK_DEBUG=1
 set FLASK_RUN_HOST=0.0.0.0
-SET FLASK_RUN_PORT=8080
-flask run
+SET FLASK_RUN_PORT=80
+
+D:\Software\flask\Scripts\flask.exe run
