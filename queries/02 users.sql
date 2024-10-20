6248d6f9033370319a52a57a21446ee7fcfa0c2e
mahdevar <mahdevar@kashanu.ac.ir>
2024-10-20 11:51:10 +0330

    تنها کاربر «admin» در آغاز افزوده می‌شود.

--- queries/02 users.sql
+++ queries/02 users.sql
@@ -1,8 +1,2 @@
 INSERT INTO users(id, user_name, password) VALUES
-	(1,	'admin',	'1'),
-	(2,	'mostafa',	'1'),
-	(3,	'mo',	'6LL1IARP2EO4OL2N9KCC53A6SOSL8A5B') ON CONFLICT DO NOTHING;
-INSERT INTO shops(id,	owner,	title) VALUES
-	(1,	2,	'آرایشگاه مصطفی'),
-	(2,	2,	'مصفا'),
-	(3,	3,	'مو به مو') ON CONFLICT DO NOTHING;
+	(0,	'admin',	'6LL1IARP2EO4OL2N9KCC53A6SOSL8A5B') ON CONFLICT DO NOTHING;
