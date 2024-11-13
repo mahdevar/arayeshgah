e2d00b6d844f56580b45f03d188c0c669cb029e9
mahdevar <mahdevar@kashanu.ac.ir>
2024-11-13 17:16:55 +0330

    پرونده‌ای پر از ریزه‌کاری‌ها

---
+++ config.py
@@ -0,0 +1,9 @@
+CSP_DIRECTIVES = \
+{
+	'connect-src': ['geolocation-db.com'],
+	'font-src': ['fonts.gstatic.com'],
+	'frame-ancestors': [],
+	'img-src': [],
+	'script-src': [],
+	'style-src': ['fonts.googleapis.com']
+}
