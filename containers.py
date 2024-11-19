30e8bdd9b2a5d5c35b1c4472f237b3fe8fe19a94
mahdevar <ghmahdevar@gmail.com>
2024-11-19 18:12:00 +0330

    نام یک پایگاه داده‌ها بهبود یافت.

--- containers.py
+++ containers.py
@@ -1,9 +1,10 @@
-__all__ = ['Cache', 'Database', 'Storage', 'Users']
+__all__ = ['Cache', 'Database', 'Session', 'Storage']
+
 from functools import partial
-from redis import Redis as OriginalRedis
-from minio import Minio
 from json import dumps, loads
+from minio import Minio
 from psycopg2.pool import ThreadedConnectionPool
+from redis import Redis as OriginalRedis
 
 S = {}
 with open('.env') as file:
@@ -12,11 +13,13 @@ with open('.env') as file:
 		if line:
 			key, value = line.split('=')
 			S[key] = value
-S['STORAGE_ENDPOINT'] += ':' + S['STORAGE_PORT']  
+S['STORAGE_ENDPOINT'] += ':' + S['STORAGE_PORT']
 del S['STORAGE_PORT']
+
 
 def factory(cls, prefix, **custom):
 	return partial(cls, **{secret[len(prefix) + 1:].lower(): S[secret] for secret in S if secret.startswith(prefix)} | custom)
+
 
 class Redis(OriginalRedis):
 	def __getitem__(self, item):
@@ -24,7 +27,9 @@ class Redis(OriginalRedis):
 
 	def __setitem__(self, key, value: [float, int, str, dict]):
 		self.set(key, dumps(value))
+
+
 Cache = factory(Redis, 'CACHE')
 Database = factory(ThreadedConnectionPool, 'DB')
+Session = factory(Redis, 'SESSION')
 Storage = factory(Minio, 'STORAGE')
-Users = factory(Redis, 'USERS')
