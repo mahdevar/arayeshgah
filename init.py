1fa40b62ea86e6c694a614878ffdad6c88c5ddb0
mahdevar <ghmahdevar@gmail.com>
2024-11-19 17:26:11 +0330

    بارگیری زبان‌ها و انجام چند کار آغازین

--- init.py
+++ init.py
@@ -1,31 +1,18 @@
+from containers import Cache, Database
 from psycopg2.extras import RealDictCursor
-from containers import Cache, Database 
-from json import dumps, loads
-
-
-
-to_json = lambda data: '{}' if data is None else dumps(data, ensure_ascii=False, separators=(',', ':'))
+from utilities import to_json
 
 DB = Database()
-
-TRANSLATIONS = Cache()
-
 CONNECTION = DB.getconn()
-CURSOR = CONNECTION.cursor(cursor_factory=RealDictCursor)
-CURSOR.execute('SELECT * FROM translations WHERE id=\'LANGUAGE CODE\'')
+TRANSLATIONS = Cache()
+with CONNECTION.cursor(cursor_factory=RealDictCursor) as CURSOR:
+	CURSOR.execute('SELECT * FROM translations WHERE id=\'LANGUAGE CODE\'')
+	for language in [value for value in CURSOR.fetchone().values() if value != 'LANGUAGE CODE']:
+		print('>>>>>>>>>>>', language)
+		CURSOR.execute('SELECT id, %s FROM translations' % language)
+		pairs = {key: value for key, value in [list(k.values()) for k in CURSOR.fetchall()]}
+		with open('./file/%s.js' % language, 'w') as file:
+			print('document.translation = %s;' % to_json(pairs), file=file)
+		TRANSLATIONS[language] = pairs
 
-
-
-for language in [value for value in CURSOR.fetchone().values() if value != 'LANGUAGE CODE']:
-	print('>>', language)
-
-'''
-	CURSOR.execute('SELECT id, %s FROM translations' % language)
-	TRANSLATIONS[language] = {key: value for key, value in [list(k.values()) for k in CURSOR.fetchall()]}
-	for language, translation in TRANSLATIONS.items():
-		with open('./file/%s.js' % language, 'w', encoding='utf-8') as file:
-			print('document.translation = %s;' % to_json(translation), file=file)
-	G['LANGUAGES'] = TRANSLATIONS.keys()
-	POOL.putconn(g.connection)
-
-'''
+DB.putconn(CONNECTION)
