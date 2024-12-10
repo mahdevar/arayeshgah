7caf25532b86a8bd892a53f7b2d3e9f4a5ffa29f
Mahdevar <ghmahdevar@gmail.com>
2024-12-10 16:21:24 +0330

    دو سه فرایند پس و پیش شدند.

--- utilities.py
+++ utilities.py
@@ -1,21 +1,40 @@
-__all__ = ['to_json']
+__all__ = ['hash_string', 'HTTP', 'jsonify', 'response', 'uuid']
 
 from annotation import Dictionary, Number, String
-from threading import Event
+from base64 import b32hexencode
+from flask import blueprints, Response
 from functools import wraps
 from hashlib import sha1
-from os import urandom
-from flask import blueprints, Response
-from base64 import b32hexencode
 from json import dumps
-#from flask.wrappers import Response
+from os import urandom
+from threading import Event
 
 
-def to_json(data: Dictionary | Number | String) -> String:
+# from flask.wrappers import Response
+def jsonify(data: Dictionary | Number | String) -> String:
 	if data:
 		return dumps(data, ensure_ascii=False, separators=(',', ':'))
 	else:
 		return '{}'
+
+
+def response(data):
+	return Response(jsonify(data), mimetype='application/json')
+
+
+EMPTY = ''  # json_response({})
+
+
+class HTTP:
+	ACCEPTED = EMPTY, 202
+	CONFLICT = EMPTY, 409
+	CREATED = EMPTY, 201
+	FORBIDDEN = EMPTY, 403
+	METHOD_NOT_ALLOWED = EMPTY, 405
+	NOT_ACCEPTABLE = EMPTY, 406
+	NO_CONTENT = EMPTY, 204
+	OK = EMPTY, 200
+	UNAUTHORIZED = EMPTY, 401
 
 
 def routing(blueprints, **m):
@@ -33,50 +52,31 @@ def routing(blueprints, **m):
 	return decorator
 
 
+'''
+# Simple scheduler
 __STOP_SCHEDULER__ = Event()
 __SCHEDULED_FUNCTIONS__ = []
-
-
 def schedule(interval):
 	def decorator(function):
 		@wraps(function)
 		def repeater(*args, **kwargs):
 			while not __STOP_SCHEDULER__.wait(interval):
 				function(*args, **kwargs)
-
 		__SCHEDULED_FUNCTIONS__.append(repeater)
 		return repeater
-
 	return decorator
-
-'''
 @schedule(10 * ALLOWED_INACTIVITY)
 def log_inactive_users_out():
 	inactive_users = [user for user, data in USERS.items() if time() - data['last activity time'] > ALLOWED_INACTIVITY]
 	for user in inactive_users:
 		del USERS[user]
 '''
-
 '''
 # Start scheduled tasks
 for scheduled_function in __SCHEDULED_FUNCTIONS__:
 	Thread(target=scheduled_function, daemon=True).start()
 run_at_exit(__STOP_SCHEDULER__.set)
 '''
-
-EMPTY = ''  # json_response({})
-
-
-class HTTP:
-	ACCEPTED = EMPTY, 202
-	CONFLICT = EMPTY, 409
-	CREATED = EMPTY, 201
-	FORBIDDEN = EMPTY, 403
-	METHOD_NOT_ALLOWED = EMPTY, 405
-	NOT_ACCEPTABLE = EMPTY, 406
-	NO_CONTENT = EMPTY, 204
-	OK = EMPTY, 200
-	UNAUTHORIZED = EMPTY, 401
 
 
 # Utility functions
@@ -88,5 +88,8 @@ def uuid(prefix=''):
 	return prefix + b32hexencode(urandom(20)).decode()
 
 
-def response(data):
-	return Response(to_json(data), mimetype='application/json')
+'''
+def get_nearby_requests():
+	# SELECT ST_ASTEXT(T.LOC) AS POINT,	ST_DISTANCE(LOC, POINT(49, 49)) AS DIST FROM T ORDER BY ST_DISTANCE(LOC, POINT(49, 49)) LIMIT 2;
+	read_table('SELECT ST_ASTEXT(T.LOC) AS POINT, ST_DISTANCE(LOC, POINT(49, 49)) AS DIST FROM T ORDER BY ST_DISTANCE(LOC, POINT(49, 49)) LIMIT 2')
+'''
