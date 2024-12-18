149bb671ca30f47468830f32b843ba67791cd0c3
mahdevar <ghmahdevar@gmail.com>
2024-12-18 16:07:05 +0330

    ساده سازی

--- initialize.py
+++ initialize.py
@@ -1,24 +1,23 @@
 from containers import Cache, Database
-from psycopg2.extras import RealDictCursor
+from psycopg.rows import dict_row
 from utilities import jsonify
 
 
 def load_languages():
-	db = Database()
-	connection = db.getconn()
 	translations = Cache()
-	with connection.cursor(cursor_factory=RealDictCursor) as cursor:
+	with db.connection() as connection, connection.cursor() as cursor:
 		cursor.execute('SELECT * FROM translations WHERE id=\'LANGUAGE CODE\'')
-		languages = [value for value in cursor.fetchone().values() if value != 'LANGUAGE CODE']
+		languages = cursor.fetchone()[1:]
+		print('ALL:', languages)
 		translations['languages'] = languages
 		for language in languages:
 			print('>>>>>>>>>>>', language)
 			cursor.execute('SELECT id, %s FROM translations' % language)
-			pairs = {key: value for key, value in [list(k.values()) for k in cursor.fetchall()]}
+			pairs = {key: value for key, value in cursor.fetchall()}
+			print('????', language, pairs)
 			with open('./static/%s.js' % language, 'w') as file:
 				print('document.translation = %s;' % jsonify(pairs), file=file)
 			translations[language] = pairs
-	db.putconn(connection)
-
+	print('>>>>>>>DONE')
 
 load_languages()
