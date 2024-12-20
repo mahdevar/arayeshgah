04517130633995dc4c2a30b02cc3effc09c95a9e
mahdevar <ghmahdevar@gmail.com>
2024-12-21 01:48:43 +0330

    چند دستور که برای یافن خطا‌ها بودند خاموش شدند

--- initialize.py
+++ initialize.py
@@ -9,16 +9,16 @@ def load_languages():
 	with db.connection() as connection, connection.cursor() as cursor:
 		cursor.execute('SELECT * FROM translations WHERE id=\'LANGUAGE CODE\'')
 		languages = cursor.fetchone()[1:]
-		print('ALL:', languages)
+		#print('ALL:', languages)
 		translations['languages'] = languages
 		for language in languages:
-			print('>>>>>>>>>>>', language)
+			#print('>>>>>>>>>>>', language)
 			cursor.execute('SELECT id, %s FROM translations' % language)
 			pairs = {key: value for key, value in cursor.fetchall()}
-			print('????', language, pairs)
+			#print('????', language, pairs)
 			with open('./static/%s.js' % language, 'w') as file:
 				print('document.translation = %s;' % jsonify(pairs), file=file)
 			translations[language] = pairs
-	print('>>>>>>>DONE')
+	#print('>>>>>>>DONE')
 
 load_languages()
