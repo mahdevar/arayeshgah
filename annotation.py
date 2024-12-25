c6e55a12f06755e6d7f0c88bf6bb6cdfa3bc6692
Mahdevar <ghmahdevar@gmail.com>
2024-12-25 13:11:29 +0330

    گونه Cursor هم به این پرونده افزوده شد.

--- annotation.py
+++ annotation.py
@@ -1,6 +1,7 @@
-__all__ = ['Any', 'Bool', 'Class', 'Dictionary', 'Generator', 'List', 'Number', 'Serializable', 'String', 'TypeVar']
+__all__ = ['Any', 'Bool', 'Class', 'Cursor', 'Dictionary', 'Generator', 'List', 'Number', 'Serializable', 'String', 'TypeVar']
 
 from typing import Any, Callable, Generator, TypeVar
+from psycopg import Cursor
 
 Bool = bool
 Class = TypeVar('Class', bound=Callable)
