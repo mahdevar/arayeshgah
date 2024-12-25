a45858c75ac178f592f3aabce7f311203c9a37dd
Mahdevar <ghmahdevar@gmail.com>
2024-12-25 10:31:16 +0330

    گونه Bool هم به این پرونده افزوده شد.

--- annotation.py
+++ annotation.py
@@ -1,7 +1,8 @@
-__all__ = ['Any', 'Class', 'Dictionary', 'Generator', 'Number', 'Serializable', 'String', 'TypeVar', 'Union']
+__all__ = ['Any', 'Bool', 'Class', 'Dictionary', 'Generator', 'Number', 'Serializable', 'String', 'TypeVar', 'Union']
 
 from typing import Any, Callable, Generator, TypeVar, Union
 
+Bool = bool
 Class = TypeVar('Class', bound=Callable)
 Dictionary = dict
 Number = float | int
