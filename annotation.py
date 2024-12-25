696447a4d4ba397038b7ee711ded1f44b3ead9ff
Mahdevar <ghmahdevar@gmail.com>
2024-12-25 11:50:09 +0330

    نیازی به گونه Bool و Generator نبود!

--- annotation.py
+++ annotation.py
@@ -1,10 +1,10 @@
-__all__ = ['Any', 'Bool', 'Class', 'Dictionary', 'Generator', 'Number', 'Serializable', 'String', 'TypeVar', 'Union']
+__all__ = ['Any', 'Class', 'Dictionary', 'List', 'Number', 'Serializable', 'String', 'TypeVar']
 
-from typing import Any, Callable, Generator, TypeVar, Union
+from typing import Any, Callable, TypeVar
 
-Bool = bool
 Class = TypeVar('Class', bound=Callable)
 Dictionary = dict
+List = list
 Number = float | int
 Serializable = dict | float | int | str
 String = str
