bcb3b99727377bfc266a49e517022127a1f9d46f
Mahdevar <ghmahdevar@gmail.com>
2024-12-29 11:01:30 +0330

    فرایند row باید یک و تنها یک پاسخ داشته باشد!

--- containers.py
+++ containers.py
@@ -1,10 +1,11 @@
 __all__ = ['Cache', 'Database', 'Session', 'Storage']
 
-from annotation import Class, Dictionary, List, Number, Serializable, String
+from annotation import Class, Dictionary, List, Number, Parameter, Serializable, String
 from atexit import register as run_at_exit
 from functools import partial
 from json import dumps, loads
 from minio import Minio
+from psycopg import IntegrityError
 from psycopg.rows import dict_row
 from psycopg_pool import ConnectionPool
 from redis import Redis as OriginalRedis
@@ -35,20 +36,22 @@ class DBPool(ConnectionPool):
 		super().__init__(min_size=1, max_size=100, kwargs=p | {'row_factory': dict_row})
 		run_at_exit(self.close)
 
-	def commit(self, query: String, parameters: List | None = None) -> None:
+	def commit(self, query: String, parameters: Parameter = None) -> None:
 		with self.connection() as connection:
 			connection.execute(query, parameters)
 
-	def commits(self, query: String, parameters: List | None = None) -> None:
+	def commits(self, query: String, parameters: Parameter = None) -> None:
 		with self.connection() as connection:
 			connection.executemany(query, parameters)
 
-	def row(self, query: String, parameters: List | None = None) -> Dictionary | None:
+	def row(self, query: String, parameters: Parameter = None) -> Dictionary | None:
 		with self.connection() as connection, connection.cursor() as cursor:
 			cursor.execute(query, parameters)
+			if cursor.rowcount > 1:
+				raise IntegrityError
 			return cursor.fetchone()
 
-	def rows(self, query: String, parameters: List | None = None) -> List:
+	def rows(self, query: String, parameters: Parameter = None) -> List:
 		with self.connection() as connection, connection.cursor() as cursor:
 			cursor.execute(query, parameters)
 			return cursor.fetchall()
