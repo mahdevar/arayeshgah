af9c57081f0f8003be4079a025675a5710369439
Mahdevar <ghmahdevar@gmail.com>
2024-12-29 11:02:16 +0330

    گونه Parameter هم به این پرونده افزوده شد.

--- annotation.py
+++ annotation.py
@@ -1,11 +1,14 @@
-__all__ = ['Any', 'Bool', 'Class', 'Dictionary', 'List', 'Number', 'Serializable', 'String', 'TypeVar']
+__all__ = ['Any', 'Bool', 'Class', 'Dictionary', 'List', 'Number', 'Parameter', 'Serializable', 'String', 'TypeVar']
 
 from typing import Any, Callable, TypeVar
+
+from click import Parameter
 
 Bool = bool
 Class = TypeVar('Class', bound=Callable)
 Dictionary = dict
 List = list
 Number = float | int
+Parameter = list | tuple | None
 Serializable = dict | float | int | str
 String = str
