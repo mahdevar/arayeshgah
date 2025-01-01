5d168d270f4c99bcd54b49ae8681d794b721ce39
Mahdevar <ghmahdevar@gmail.com>
2025-01-01 17:03:04 +0330

    برگردان نوشته‌ها به زبان‌های گوناگون درست از پایگاه داده‌ها خوانده و در پرونده‌هایی که باید نوشته می‌شوند.

--- initialize.py
+++ initialize.py
@@ -1,24 +1,16 @@
 from containers import Cache, Database
-from psycopg.rows import dict_row
 from utilities import jsonify
 
 
-def load_languages():
+def load_languages() -> None:
 	db = Database()
 	translations = Cache()
-	with db.connection() as connection, connection.cursor() as cursor:
-		cursor.execute('SELECT * FROM translations WHERE id=\'LANGUAGE CODE\'')
-		languages = cursor.fetchone()[1:]
-		#print('ALL:', languages)
-		translations['languages'] = languages
-		for language in languages:
-			#print('>>>>>>>>>>>', language)
-			cursor.execute('SELECT id, %s FROM translations' % language)
-			pairs = {key: value for key, value in cursor.fetchall()}
-			#print('????', language, pairs)
-			with open('./static/%s.js' % language, 'w') as file:
-				print('document.translation = %s;' % jsonify(pairs), file=file)
-			translations[language] = pairs
-	#print('>>>>>>>DONE')
+	languages = [code for code in db.row('SELECT * FROM translations WHERE id=%s', ['LANGUAGE CODE']) if code != 'id']
+	for language in languages:
+		pairs = {pair['id']: pair[language] for pair in db.rows('SELECT id, %s FROM translations' % language)}
+		with open('static/%s.js' % language, 'w') as file:
+			print('document.translation = %s;' % jsonify(pairs), file=file)
+		translations[language] = pairs
+
 
 load_languages()
